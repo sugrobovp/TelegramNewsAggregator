117ed3dc5f7849947fc72535db6efd2ef480e469
Peter <petrucho9@gmail.com>
2023-04-11 15:18:43 +0300

    Worked out interface for Parsers with DIP principle

--- scrapper.py
+++ scrapper.py
@@ -2,54 +2,56 @@ from datetime import datetime
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from abc import ABC, abstractmethod
+from typing import Type
 
 
-class Browser:
+# class Browser:
+#
+#     def __init__(self, url):
+#         self.url = url
+#         self.driver = webdriver.Chrome()
+#         self.html = None
+#
+#     def start(self):
+#         self.driver.get(self.url)
+#         self.html = self.driver.page_source
+#
+#     def stop(self):
+#         self.driver.quit()
 
-    def __init__(self, url):
-        self.url = url
-        self.driver = webdriver.Chrome()
-        self.html = None
 
-    def start(self):
-        self.driver.get(self.url)
-        self.html = self.driver.page_source
-
-    def stop(self):
-        self.driver.quit()
-
-
-class HTMLParserInterface(ABC):
+class ParserInterface(ABC):
 
     @abstractmethod
-    def parse(self, html) -> None:
+    def parse(self, url):
         pass
 
 
-class BeautifulSoupHTMLParser(HTMLParserInterface):
+class BeautifulSoupHTMLParser(ParserInterface):
 
-    def parse(self, html) -> None:
-        pass
+    @classmethod
+    def parse(cls, url):
+        driver = webdriver.Chrome()
+        driver.get(url)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        driver.quit()
+        return soup
 
 
-class CybersportScrapper:
-    # Задаем URL сайта
-    url = 'https://www.cybersport.ru/'
+class CybersportParser:
 
-    # Запускаем браузер
-    driver = webdriver.Chrome()
-    driver.get(url)
+    def __init__(self, parser: Type[BeautifulSoupHTMLParser]):
+        self.parser = parser
 
-    # Получаем содержимое страницы
-    html = driver.page_source
+    def parse(self, url):
+        soup = self.parser.parse(url=url)
+        data = self.extract_data(soup)
+        return data
 
-    # Создаем объект BeautifulSoup для парсинга HTML
-    soup = BeautifulSoup(html, 'html.parser')
-
-    def get_news(self):
-
-        # Находим все блоки с новостями
-        news_blocks = self.soup.find_all('div', class_='rounded-block root_d51Rr with-hover no-padding no-margin')
+    @staticmethod
+    def extract_data(soup):
+        news_blocks = soup.find_all('div', class_='rounded-block root_d51Rr with-hover no-padding no-margin')
 
         results = []
 
@@ -85,7 +87,3 @@ class CybersportScrapper:
             print()
 
         return results
-
-
-    # Закрываем браузер
-    driver.quit()
